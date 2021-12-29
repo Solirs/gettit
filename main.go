77d82ee2bbb1c62e2a712b85976f179819f6bceb
solirs <zcauchemar@gmail.com>
2021-12-29 22:19:09 +0100

    GOFMTe'd and added a function to fix urls

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/tidwall/gjson" //For json parsing
 	"io"
 	"io/ioutil"
 	"log"
@@ -10,9 +11,9 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"time"
 	"regexp"
-	"github.com/tidwall/gjson" //For json parsing
+	"strings"
+	"time"
 )
 
 var url string
@@ -25,21 +26,15 @@ var videofile string
 
 var audiofile string
 
-
 var Red = "\033[31m"
-
 
 var Green = "\033[32m"
 
-
 var Yellow = "\033[33m"
-
 
 var Blue = "\033[34m"
 
-
 var Purple = "\033[35m"
-
 
 var Cyan = "\033[36m"
 
@@ -47,51 +42,63 @@ var White = "\033[37m"
 
 var Reset = "\033[0m"
 
-func initflags(){
+func initflags() {
 	flag.StringVar(&url, "u", "NONE", "Url of the post")
 	flag.BoolVar(&noclean, "noclean", false, "Don't remove separate audio and video files after merging them")
 	flag.Parse()
 }
 
-func checkerror(err error){
-	if err != nil{
+func checkerror(err error) {
+	if err != nil {
 		log.Fatal(err)
 	}
 
 }
 
-func main(){
+func correcturl() {
 
+	/* This function will attempt to turn the url into a .json*/
 
+	if strings.HasSuffix(url, ".json") {
+		return
+	}
+	if strings.HasSuffix(url, "/") {
+		url = url[:len(url)-1]
+		url = fmt.Sprint(url, ".json")
+	}
 
+}
+
+func main() {
 
 	fmt.Println("Initializing...")
 
 	initflags()
 
-	if url == "NONE"{
+	correcturl()
+
+	if url == "NONE" {
 		log.Fatal(Red, "No post URL specified. Quitting.", Reset)
 
 	}
-	
 
+	fmt.Println(strings.TrimSuffix(url, "+"))
 
-	req, err := http.NewRequest("GET", url, nil)	
-	
+	req, err := http.NewRequest("GET", url, nil)
+
 	checkerror(err)
-
 
 	req.Header.Set("User-Agent", "Mozilla/5.0")
 
 	resp, err := new(http.Client).Do(req)
-	
+
 	checkerror(err)
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
 	fmt.Println("Fetching url of the source video file...")
 
-    body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 
 	checkerror(err)
 
@@ -99,14 +106,11 @@ func main(){
 
 	checkerror(err)
 
-
-    fmt.Println(video.String())
+	fmt.Println(video.String())
 
 	fmt.Println("Downloading source video file...")
 
 	DLfile(video.String(), "video")
-
-
 
 	fmt.Println("Downloading audio...")
 
@@ -120,8 +124,7 @@ func main(){
 
 	fmt.Println("Merging audio and video...")
 
-	args := []string{"-i" ,videofile, "-i" ,audiofile, "-c:v", "copy", "-c:a" , "aac" , "out.mp4"}
-
+	args := []string{"-i", videofile, "-i", audiofile, "-c:v", "copy", "-c:a", "aac", "out.mp4"}
 
 	cmd := exec.Command("ffmpeg", args...)
 
@@ -130,35 +133,32 @@ func main(){
 	err = cmd.Run()
 	checkerror(err)
 
-	if !noclean{
+	if !noclean {
 		fmt.Println("\nCleaning...")
 
 		err = os.Remove(audiofile)
 		checkerror(err)
-	
+
 		err = os.Remove(videofile)
 		checkerror(err)
 
 	}
 
-
-
-
-
 }
 
-func DLfile(url string, saveas string){
+func DLfile(url string, saveas string) {
+
+	/* This function will download an mp4 file and save the file names in a variable to merge them later */
 
 	rand.Seed(time.Now().UnixNano())
 
-	req, err := http.NewRequest("GET", url, nil)	
+	req, err := http.NewRequest("GET", url, nil)
 
 	checkerror(err)
 
 	req.Header.Set("User-Agent", "Mozilla/5.0")
 
-
-	fil, err := new(http.Client).Do(req)	
+	fil, err := new(http.Client).Do(req)
 
 	checkerror(err)
 
@@ -177,9 +177,9 @@ func DLfile(url string, saveas string){
 	_, err = io.Copy(file, fil.Body)
 	checkerror(err)
 
-	if saveas == "video"{
-		videofile = string(RandomName) +  ".mp4"
-	}else{
+	if saveas == "video" {
+		videofile = string(RandomName) + ".mp4"
+	} else {
 		audiofile = string(RandomName) + ".mp4"
 	}
 
